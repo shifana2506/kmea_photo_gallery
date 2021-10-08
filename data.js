5680b305b2c50dcb06d4f84cae5027517d56fa74
Anju Gopan <56990161+Anju-Gopan@users.noreply.github.com>
2021-10-08 15:18:08 +0530

    Update data.js

--- data.js
+++ data.js
@@ -4,6 +4,12 @@ const data = {
         lname: "joseph",
         dept: "Computer science and engineering",
         NumberOfPhotos: 2,
+    },
+    anju: {
+        fname: "anju",
+        lname: "gopan",
+        dept: "Computer science and engineering",
+        NumberOfPhotos: 2,
     }
    }
 export default data;
